93061f1ba6ea3151558abefa75ca232d5743b4b9
Joycrown <olaniyanayoade@ymail.com>
2021-07-02 11:11:30 +0100

    modification

--- crownpack/settings.py
+++ crownpack/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crownpack_app.apps.CrownpackAppConfig',
-    'bootstrap4',
+    'django-bootstrap5',
     
 ]
 
